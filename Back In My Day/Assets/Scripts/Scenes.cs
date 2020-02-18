6b6fcc95ac350568459a69ca8e0ffcb3ad649c35
E2-Waite <E2-Waite>
2020-02-18 22:38:40 +0000

    Started Implementing Scene Manager and Greatly Improved Trinity Lightmap
    Improved Trinity model and lightmap. Also started to implement the scene manager that will be used to seamlessly transition between the game scenes. Currently switches between Trinity and main menu placeholder.

---
+++ Back In My Day/Assets/Scripts/Scenes.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class Scenes : MonoBehaviour
+{
+    SceneManager manager;
+    bool scene_loaded = false;
+    Scene menu_scene;
+    Scene trinity;
+    Scene fleece;
+    Scene lakota;
+    Scene cosies;
+    Scene current_scene;
+    // Start is called before the first frame update
+    void Start()
+    {
+        menu_scene = SceneManager.GetActiveScene();
+        current_scene = SceneManager.GetActiveScene();
+        SceneManager.LoadScene("Trinity", LoadSceneMode.Additive);
+        SceneManager.LoadScene("Fleece", LoadSceneMode.Additive);
+        SceneManager.LoadScene("Lakota", LoadSceneMode.Additive);
+        SceneManager.LoadScene("Cosies", LoadSceneMode.Additive);
+        trinity = SceneManager.GetSceneByName("Trinity");
+        fleece = SceneManager.GetSceneByName("Fleece");
+        lakota = SceneManager.GetSceneByName("Lakota");
+        cosies = SceneManager.GetSceneByName("Cosies");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyUp("1") && !scene_loaded)
+        {
+            if (current_scene.name == "SampleScene")
+            {
+                SceneManager.SetActiveScene(trinity);
+                current_scene = SceneManager.GetActiveScene();
+            }
+            else if (current_scene.name == "Trinity")
+            {
+                SceneManager.SetActiveScene(menu_scene);
+                current_scene = SceneManager.GetActiveScene();
+            }
+        }
+    }
+}
