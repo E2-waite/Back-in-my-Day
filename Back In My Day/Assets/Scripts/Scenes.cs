e52099f721cf9fe2e805d5fc4d9a9d32c0bb7844
E2-Waite <E2-Waite>
2020-02-19 16:28:54 +0000

    Basic Working Scene Manager (Removed Trinity Lightmap)
    Working scene manager that transitions between loaded scenes (fades to black, then fades into other scene). Needs to be improved and needs other scenes to be implemented.
    Removed Trinity light-map (keep needing to re-bake, takes ages). Will add back later.

--- Back In My Day/Assets/Scripts/Scenes.cs
+++ Back In My Day/Assets/Scripts/Scenes.cs
@@ -4,44 +4,71 @@ using UnityEngine;
 using UnityEngine.SceneManagement;
 public class Scenes : MonoBehaviour
 {
-    SceneManager manager;
-    bool scene_loaded = false;
-    Scene menu_scene;
-    Scene trinity;
-    Scene fleece;
-    Scene lakota;
-    Scene cosies;
-    Scene current_scene;
+    public GameObject player;
+    public GameObject screen_cover;
+    ScreenFade fade;
+    public List<string> scene_names = new List<string>();
+    List<Scene> scenes = new List<Scene>();
+    public List<GameObject> scene_obj = new List<GameObject>();
     // Start is called before the first frame update
+
+
     void Start()
     {
-        menu_scene = SceneManager.GetActiveScene();
-        current_scene = SceneManager.GetActiveScene();
-        SceneManager.LoadScene("Trinity", LoadSceneMode.Additive);
-        SceneManager.LoadScene("Fleece", LoadSceneMode.Additive);
-        SceneManager.LoadScene("Lakota", LoadSceneMode.Additive);
-        SceneManager.LoadScene("Cosies", LoadSceneMode.Additive);
-        trinity = SceneManager.GetSceneByName("Trinity");
-        fleece = SceneManager.GetSceneByName("Fleece");
-        lakota = SceneManager.GetSceneByName("Lakota");
-        cosies = SceneManager.GetSceneByName("Cosies");
+        fade = screen_cover.GetComponent<ScreenFade>();
+        StartCoroutine(SetupScenes());
     }
 
-    // Update is called once per frame
-    void Update()
+    IEnumerator SetupScenes()
     {
-        if (Input.GetKeyUp("1") && !scene_loaded)
+        for (int i = 0; i < scene_names.Count; i++)
         {
-            if (current_scene.name == "SampleScene")
-            {
-                SceneManager.SetActiveScene(trinity);
-                current_scene = SceneManager.GetActiveScene();
-            }
-            else if (current_scene.name == "Trinity")
-            {
-                SceneManager.SetActiveScene(menu_scene);
-                current_scene = SceneManager.GetActiveScene();
-            }
+            SetupScene(scene_names[i]);
+            yield return null;
         }
+        for (int i = 0; i < scene_names.Count; i++)
+        {
+            SetupObject(scene_names[i]);
+            yield return null;
+        }
+    }
+
+    void SetupScene(string scene)
+    {
+        SceneManager.LoadScene(scene, LoadSceneMode.Additive);
+        scenes.Add(SceneManager.GetSceneByName(scene));
+    }
+
+    void SetupObject(string scene)
+    {
+        if (scene != "Menu")
+        {
+            scene_obj.Add(GameObject.Find(scene));
+            scene_obj[scene_obj.Count - 1].SetActive(false);
+        }
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyUp("1") && !fade.fading)
+        {
+            StartCoroutine(ActivateScene(0));
+        }
+    }
+
+    IEnumerator ActivateScene(int num)
+    {
+        StartCoroutine(fade.FadeIn());
+        yield return null;
+        while (fade.fading) { yield return null; }
+        scene_obj[num].SetActive(true);
+        yield return new WaitForSeconds(1);
+        StartCoroutine(fade.FadeOut());
+    }
+
+    IEnumerator DeactivateScene(int num)
+    {
+        scene_obj[num].SetActive(false);
+        yield return null;
     }
 }
